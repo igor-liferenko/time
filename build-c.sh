019b64dd12086bf8c95e51bb6689bd4051d3796d
Igor Liferenko <igor.liferenko@gmail.com>
2024-03-12 18:32:24 +0700

    edit

--- build-c.sh
+++ build-c.sh
@@ -2,41 +2,39 @@
 
 grep '[S]SID\|[K]EY' $0 && exit
 
-IMG=openwrt-imagebuilder-23.05.2-bcm27xx-bcm2710.Linux-x86_64
-SDK=openwrt-sdk-23.05.2-bcm27xx-bcm2710_gcc-12.3.0_musl.Linux-x86_64
-URL=https://downloads.openwrt.org/releases/23.05.2/targets/bcm27xx/bcm2710
-mkdir -p ~/openwrt
-cd ~/openwrt
+IMG=lede-imagebuilder-17.01.7-brcm47xx-legacy.Linux-x86_64
+SDK=lede-sdk-17.01.7-brcm47xx-legacy_gcc-5.4.0_musl-1.1.16.Linux-x86_64
+URL=https://downloads.openwrt.org/releases/17.01.7/targets/brcm47xx/legacy
+mkdir -p ~/lede
+cd ~/lede
 [ -e $IMG.tar.xz ] || wget $URL/$IMG.tar.xz || exit
 [ -e $SDK.tar.xz ] || wget $URL/$SDK.tar.xz || exit
-rm -fr c/
-mkdir c/
-cd c/
+rm -fr k/
+mkdir k/
+cd k/
 tar -Jxf ../$IMG.tar.xz
 tar -Jxf ../$SDK.tar.xz
 cd $SDK/
 ctangle ~/time/time-write.w || exit
-sed -i 's@/dev/ttyACM0@/dev/serial/by-id/usb-03eb_2018-if00@' time-write.c
-STAGING_DIR=~/openwrt/c/$SDK/staging_dir/toolchain* ./staging_dir/toolchain*/bin/aarch64-openwrt-linux-gcc time-write.c -o time-write || exit
+STAGING_DIR=~/lede/k/$SDK/staging_dir/toolchain* ./staging_dir/toolchain*/bin/mipsel-openwrt-linux-gcc time-write.c -o time-write || exit
 cd ../$IMG/
 mkdir -p files/etc/uci-defaults/
 cat <<'EOF' >files/etc/uci-defaults/my
+uci set network.lan.ipaddr=192.168.1.3
+uci del network.lan.ifname
+uci del network.lan.type
+uci commit network
 uci set wireless.radio0.disabled=0
 uci set wireless.radio0.txpower=3
+uci set wireless.default_radio0.mode=sta
 uci set wireless.default_radio0.ssid=SSID
 uci set wireless.default_radio0.encryption=psk2
 uci set wireless.default_radio0.key=KEY
 uci commit wireless
-uci set gpsd.core.enabled=1
-uci set gpsd.core.device=/dev/serial/by-id/usb-u-blox_AG_-_www.u-blox.com_u-blox_7_-_GPS_GNSS_Receiver-if00 # to see if it works, use `gpspipe -r'
-uci commit gpsd
-# NOTE: ntpd.conf is generated dynamically by /etc/init.d/ntpd
-sed -i '/for i in $server/i\
-emit "server 127.127.28.0"\
-emit "fudge 127.127.28.0 flag1 1"' /etc/init.d/ntpd
-uci del system.ntp.server # to make $server empty in /etc/init.d/ntpd
-uci set system.ntp.enable_server=1 # to make $enable_server non-zero in /etc/init.d/ntpd
+uci set dhcp.lan.ignore=1
+uci commit dhcp
 uci set system.@system[0].timezone=GMT-7
+uci set system.ntp.server=192.168.1.1 # address of raspberry (gps)
 uci commit system
 EOF
 
@@ -46,6 +44,14 @@ cp ../$SDK/time-write files/bin/
 mkdir -p files/etc/
 cat <<'EOF' >files/etc/rc.local
 time-write &
+cat <<'FOE' | sh &
+sleep 60
+while [ 1 ]; do
+  ### uncomment this manually after you flash the router
+  # ping -c1 `uci get system.ntp.server` >/dev/null || reboot # to reconnect wifi automatically
+  sleep 5
+done
+FOE
 exit 0
 EOF
 
@@ -56,12 +62,9 @@ cat <<'EOF' >files/etc/crontabs/root
 0 4 * * * stty -F /dev/serial/by-id/usb-0* 110
 EOF
 
-make image PROFILE=rpi-3 PACKAGES="gpsd-clients gpsd ntpd kmod-usb-acm coreutils-stty" FILES=files/
+make image PROFILE=Broadcom-b43 PACKAGES="kmod-usb-ohci kmod-usb-acm coreutils-stty" FILES=files/
 { RET=$?; } 2>/dev/null
 { set +x; } 2>/dev/null
-gunzip bin/*/*/*/*-ext4-factory.img.gz
 if [ $RET = 0 ]; then
-  echo
-  echo 'First umount and then do (changing sdX to proper name):'
-  echo '  dd if=`ls ~/openwrt/c/*/bin/*/*/*/*-ext4-factory.img` of=/dev/sdX bs=4M; sync'
+  ls ~/lede/k/*-imagebuilder-*/bin/*/*/*/*-standard-squashfs.trx # mtd -r write /tmp/fw.img firmware
 fi

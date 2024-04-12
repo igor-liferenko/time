e5a4cfe27ca849b8b347b70aee0205e69a7a4f48
Igor Liferenko <igor.liferenko@gmail.com>
2024-04-12 21:40:49 +0700

    use openwrt

--- build-u.sh
+++ build-u.sh
@@ -2,11 +2,11 @@
 
 [ $# = 2 ] || exit
 
-IMG=lede-imagebuilder-17.01.7-ar71xx-generic.Linux-x86_64
-SDK=lede-sdk-17.01.7-ar71xx-generic_gcc-5.4.0_musl-1.1.16.Linux-x86_64
-URL=https://downloads.openwrt.org/releases/17.01.7/targets/ar71xx/generic
-mkdir -p ~/lede
-cd ~/lede
+IMG=openwrt-imagebuilder-18.06.1-ar71xx-generic.Linux-x86_64
+SDK=openwrt-sdk-18.06.1-ar71xx-generic_gcc-7.3.0_musl.Linux-x86_64
+URL=https://downloads.openwrt.org/releases/18.06.1/targets/ar71xx/generic
+mkdir -p ~/openwrt
+cd ~/openwrt
 [ -e $IMG.tar.xz ] || wget $URL/$IMG.tar.xz || exit
 [ -e $SDK.tar.xz ] || wget $URL/$SDK.tar.xz || exit
 rm -fr u/
@@ -16,7 +16,7 @@ tar -Jxf ../$IMG.tar.xz
 tar -Jxf ../$SDK.tar.xz
 cd $SDK/
 ctangle ~/time/time-write.w || exit
-STAGING_DIR=~/lede/u/$SDK/staging_dir/toolchain* ./staging_dir/toolchain*/bin/mips-openwrt-linux-gcc time-write.c -o time-write || exit
+LC_ALL=C.UTF-8 STAGING_DIR=~/openwrt/u/$SDK/staging_dir/toolchain* ./staging_dir/toolchain*/bin/mips-openwrt-linux-gcc time-write.c -o time-write || exit # LC_ALL=C.UTF-8 is for 18.06.1
 cd ../$IMG/
 mkdir -p files/etc/uci-defaults/
 cat <<'EOF' >files/etc/uci-defaults/my
@@ -46,5 +46,5 @@ make image PROFILE=tl-wr1043nd-v1 PACKAGES="kmod-usb-acm coreutils-stty" FILES=f
 { RET=$?; } 2>/dev/null
 { set +x; } 2>/dev/null
 if [ $RET = 0 ]; then
-  ls ~/lede/u/*/bin/*/*/*/*-sysupgrade.bin # mtd -r write /tmp/fw.img firmware
+  ls ~/openwrt/u/*/bin/*/*/*/*-sysupgrade.bin # mtd -r write /tmp/fw.img firmware
 fi

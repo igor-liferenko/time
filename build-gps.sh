067b7bf3e72a9b15fabe7fcf809d1f952f87c7f4
Igor Liferenko <igor.liferenko@gmail.com>
2024-10-11 16:29:01 +0700

    edit

--- build-gps.sh
+++ build-gps.sh
@@ -24,11 +24,12 @@ uci set gpsd.core.enabled=1
 uci set gpsd.core.device=/dev/ttyACM0 # to see if it works, use `gpspipe -r'
 uci commit gpsd
 uci set system.ntp.enable_server=1
-uci del system.ntp.server
-sed -i '/for i in $server/i\
-emit "server 127.127.28.0"\
-emit "fudge 127.127.28.0 flag1 1"' /etc/init.d/ntpd
+uci set system.ntp.server=127.127.28.0
+sed -i '/emit "server $i iburst"/c\
+  emit "server $i"\
+  emit "fudge $i flag1 1"' /etc/init.d/ntpd
 uci set system.@system[0].timezone=GMT-7
+uci set system.led_wlan2g.trigger=none
 uci commit system
 echo 5c:d9:98:1b:81:27 192.168.1.2 >>/etc/ethers
 echo f0:7d:68:83:16:eb 192.168.1.3 >>/etc/ethers
@@ -39,6 +40,7 @@ sed -i s/KEY/$2/ files/etc/uci-defaults/my
 mkdir -p files/etc/crontabs/
 cat <<'EOF' >files/etc/crontabs/root
 */10 * * * * check-dir320
+# * * * * * if ...; then alarm-on; else alarm-off; fi
 EOF
 
 mkdir -p files/bin/
@@ -56,6 +58,19 @@ for i in `cat /etc/ethers | cut -d' ' -f2`; do
 done
 EOF
 chmod +x files/bin/check-dir320
+cat <<'EOF' >files/bin/alarm-on
+#!/bin/sh
+echo timer >/sys/class/leds/tl-wr842n-v5:amber:wan/trigger
+echo 70 >/sys/class/leds/tl-wr842n-v5:amber:wan/delay_on
+echo 30 >/sys/class/leds/tl-wr842n-v5:amber:wan/delay_off
+EOF
+chmod +x files/bin/alarm-on
+cat <<'EOF' >files/bin/alarm-off
+#!/bin/sh
+echo none >/sys/class/leds/tl-wr842n-v5:amber:wan/trigger
+echo 0 >/sys/class/leds/tl-wr842n-v5:amber:wan/brightness
+EOF
+chmod +x files/bin/alarm-off
 
 make image PROFILE=tplink_tl-wr842n-v5 PACKAGES="gpsd-clients gpsd ntpd kmod-usb-acm" FILES=files/
 { RET=$?; } 2>/dev/null
